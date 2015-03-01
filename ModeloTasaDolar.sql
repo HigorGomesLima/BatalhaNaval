62a41830ca41158fe65770bc8c13587275752f66
raydelto <raydelto@gmail.com>
2015-02-28 20:50:45 -0400

    Actualizando Script base de datos

--- ModeloTasaDolar.sql
+++ ModeloTasaDolar.sql
@@ -1,37 +1,76 @@
-/*
-SQLyog Ultimate v9.02 
-MySQL - 5.6.12-log : Database - tasaremota
-*********************************************************************
-*/
+-- MySQL dump 10.13  Distrib 5.6.20, for Win32 (x86)
+--
+-- Host: localhost    Database: tasaremota
+-- ------------------------------------------------------
+-- Server version	5.6.17
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`tasaremota` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `tasaremota`;
+--
+-- Table structure for table `clientes`
+--
 
-/*Table structure for table `tasadolar` */
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `cliente_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`cliente_id`),
+  UNIQUE KEY `ip_UNIQUE` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasadolar`
+--
 
 DROP TABLE IF EXISTS `tasadolar`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tasadolar` (
   `idTasa` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tasa` decimal(10,2) NOT NULL,
   `fecha` date DEFAULT NULL,
   PRIMARY KEY (`idTasa`,`tasa`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `tasadolar` */
+--
+-- Dumping data for table `tasadolar`
+--
 
-insert  into `tasadolar`(`idTasa`,`tasa`,`fecha`) values (1,'9.15','2015-02-01'),(2,'10.15','2015-03-01'),(3,'9.15','2025-02-15'),(4,'9.20','2015-02-25');
+LOCK TABLES `tasadolar` WRITE;
+/*!40000 ALTER TABLE `tasadolar` DISABLE KEYS */;
+INSERT INTO `tasadolar` VALUES (1,1.00,'2015-02-28'),(2,2.00,'2015-02-28'),(3,3.00,'2015-02-28'),(4,3.00,'2015-02-28'),(5,5.00,'2015-02-28'),(6,6.00,'2015-02-28'),(7,11.00,'2015-02-28'),(8,123.00,'2015-02-28'),(9,3434.00,'2015-02-28'),(10,12.00,'2015-02-28'),(11,10.00,'2015-02-28');
+/*!40000 ALTER TABLE `tasadolar` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-28 20:50:28
